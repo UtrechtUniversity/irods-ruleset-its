be4d33a4568d1b7f473df49cc40993e1632fa56c
ton <ton@dellie.uu.nl>
2017-12-19 13:21:36 +0100

    blacklist added

---
+++ uuItsConstants.r
@@ -0,0 +1,15 @@
+# \file
+# \copyright Copyright (c) 2016, Utrecht university. All rights reserved
+# \license GPLv3, see LICENSE
+
+# \constant UUPRIMARYRESOURCES
+UUPRIMARYRESOURCES = list("irodsResc");
+
+# \constant UUREPLICATIONRESOURCE
+UUREPLICATIONRESOURCE = "irodsRescRepl";
+
+# \constant UUMAXREVISIONSIZE
+UUMAXREVISIONSIZE = double("2000000000"); # 2GB as in 2 * 1000 * 1000 * 1000
+
+# \constant UUBLACKLIST
+UUBLACKLIST = list("._*", ".DS_Store"); 
--- uuItsPolicies.r
+++ uuItsPolicies.r
@@ -1,17 +1,50 @@
 # \file
 # \brief iRODS policies for Yoda (changes to core.re)
 # \author Ton Smeele
+# \author Paul Frederiks
 # \copyright Copyright (c) 2016, Utrecht university. All rights reserved
 # \license GPLv3, see LICENSE
 #
 
 pep_resource_modified_post(*out) {
-   *sourceResource = $pluginInstanceName;
-   if (*sourceResource == 'irodsResc') {
-      uuReplicateAsynchronously($KVPairs.logical_path, *sourceResource, 'irodsRescRepl');
-   }
+  on(uuinlist($pluginInstanceName, UUPRIMARYRESOURCES)) {
+    *pluginInstanceName = $pluginInstanceName;
+    *KVPairs = $KVPairs;
+    uuReplicateAsynchronously(*KVPairs.logical_path, *pluginInstanceName, UUREPLICATIONRESOURCE);
+    # The rules on metadata are run synchronously and could fail. Log errors, but continue with revisions.
+    *err = errormsg(uuResourceModifiedPostResearch(*pluginInstanceName, *KVPairs), *msg);
+    if (*err < 0) {
+    	writeLine("serverLog", "*err: *msg");
+    }
+    uuResourceModifiedPostRevision(*pluginInstanceName, *KVPairs.client_user_zone, *KVPairs.logical_path, UUMAXREVISIONSIZE, UUBLACKLIST); 
+  }
+  # see issue https://github.com/irods/irods/issues/3500 below on(true) code is a hack to avoid debug messages in irods 4.1.8
+  on(true) {nop;}
 }
 
+# \brief pep_resource_rename_post
+# \param[in,out] out			This is a required parameter for Dynamic PEP's in 4.1.x releases. It is not used by this rule.
+pep_resource_rename_post(*out) {
+  on(uuinlist($pluginInstanceName, UUPRIMARYRESOURCES)) {
+    *pluginInstanceName = $pluginInstanceName;
+    *KVPairs =  $KVPairs;
+    uuResourceRenamePostResearch(*pluginInstanceName, *KVPairs);
+  }
+  # see issue https://github.com/irods/irods/issues/3500 below on(true) code is a hack to avoid debug messages in irods 4.1.8
+  on(true) {nop;}
+}
+
+# \brief pep_resource_unregister_post   
+# \param[in,out] out			This is a required parameter for Dynamic PEP's in 4.1.x releases. It is not used by this rule.
+pep_resource_unregistered_post(*out) {
+	on (uuinlist($pluginInstanceName, UUPRIMARYRESOURCES)) {
+		*pluginInstanceName = $pluginInstanceName;
+		*KVPairs = $KVPairs;
+		uuResourceUnregisteredPostResearch(*pluginInstanceName, *KVPairs);
+	}
+        # see issue https://github.com/irods/irods/issues/3500 below on(true) code is a hack to avoid debug messages in irods 4.1.8
+        on(true) {nop;}
+}
 
 #input null
 #output ruleExecOut
