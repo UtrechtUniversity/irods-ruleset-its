9ce26e861cc3fe770b4889bc4d0b54939b13c588
Ton Smeele <a.p.m.smeele@uu.nl>
2018-07-11 08:39:49 +0200

    instance name added

--- uuItsConstants.r
+++ uuItsConstants.r
@@ -12,4 +12,7 @@ UUREPLICATIONRESOURCE = "irodsRescRepl";
 UUMAXREVISIONSIZE = double("2000000000"); # 2GB as in 2 * 1000 * 1000 * 1000
 
 # \constant UUBLACKLIST
-UUBLACKLIST = list("._*", ".DS_Store"); 
+UUBLACKLIST = list("._*", ".DS_Store");
+
+# \constant UUINSTANCENAME     tijdelijk nodig bij Yoda 1.0-1.2
+UUINSTANCENAME = "its";
